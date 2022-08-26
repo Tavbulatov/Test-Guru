ec9288cce2d493bb4e859a850cacea215e15ef8c
Tavbulatov <alikhantam26@gmail.com>
2022-08-26 23:56:30 +0300

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  def create
+    super
+    flash[:notice] = "Hello #{current_user.name}"
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
