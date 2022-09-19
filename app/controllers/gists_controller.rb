5f29da70719a36765b11c221c30718562a9e7fcd
Tavbulatov <alikhantam26@gmail.com>
2022-09-19 15:45:03 +0300

    fixbug controller gists

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -2,12 +2,12 @@ class GistsController < ApplicationController
   def create
     test_passage = TestPassage.find(params[:test_passage_id])
 
-    result = GistQuestionService.new(@test_passage.current_question).call
+    result = GistQuestionService.new(test_passage.current_question).call
 
     flash = if result.url.nil?
               { notice: t('.failure') }
             else
-              Gist.create!(user: current_user, question: @test_passage.current_question, url: result.url)
+              Gist.create!(user: current_user, question: test_passage.current_question, url: result.url)
 
               { notice: t('.succes', url: result.url) }
             end
