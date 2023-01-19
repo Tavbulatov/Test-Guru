1ac70f8fd3d0a0aac5db72b8d88aeb21da87d180
Tavbulatov <alikhantam26@gmail.com>
2023-01-20 00:21:46 +0300

    install yarn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Action mailer
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'alikhandb.onrender.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
