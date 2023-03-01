db94871669773d8978f340113e48b1f24d3add0d
Tavbulatov <alikhantam26@gmail.com>
2023-03-01 16:44:21 +0300

    validates model badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,8 @@
 class Badge < ApplicationRecord
   has_and_belongs_to_many :users
+
+  validates :name, presence: true
+  validates :rule, presence: true
+  validates :rule_criterion , presence: true
+  validates :url_picture, presence: true
 end
