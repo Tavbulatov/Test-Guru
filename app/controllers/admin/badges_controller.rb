d37eada236f71da1f8dd31ba136c49b5e2642638
Tavbulatov <alikhantam26@gmail.com>
2023-03-11 16:27:49 +0300

    removed module in badge controller

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,48 +1,46 @@
 # frozen_string_literal: true
 
-module Admin
-  class BadgesController < Admin::BaseController
-    before_action :set_badge, only: %i[edit update destroy]
+class Admin::BadgesController < Admin::BaseController
+  before_action :set_badge, only: %i[edit update destroy]
 
-    def new
-      @badge = Badge.new
+  def new
+    @badge = Badge.new
+  end
+
+  def index
+    @badges = Badge.all
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+
+    if @badge.save
+      redirect_to admin_tests_path
+    else
+      render :new
     end
+  end
 
-    def index
-      @badges = Badge.all
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_tests_path
+    else
+      render :edit
     end
+  end
 
-    def create
-      @badge = Badge.new(badge_params)
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
 
-      if @badge.save
-        redirect_to admin_tests_path
-      else
-        render :new
-      end
-    end
+  private
 
-    def update
-      if @badge.update(badge_params)
-        redirect_to admin_tests_path
-      else
-        render :edit
-      end
-    end
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
 
-    def destroy
-      @badge.destroy
-      redirect_to admin_badges_path
-    end
-
-    private
-
-    def set_badge
-      @badge = Badge.find(params[:id])
-    end
-
-    def badge_params
-      params.require(:badge).permit(:name, :rule, :url_picture, :rule_criterion, :description)
-    end
+  def badge_params
+    params.require(:badge).permit(:name, :rule, :url_picture, :rule_criterion, :description)
   end
 end
