217387180017f054a9d859d74d42924f68bc7dab
Tavbulatov <alikhantam26@gmail.com>
2023-03-11 14:58:14 +0300

    delete migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_09_133425) do
+ActiveRecord::Schema.define(version: 2023_03_04_093724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2023_03_09_133425) do
     t.string "rule_criterion", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "description"
   end
 
   create_table "badges_users", id: false, force: :cascade do |t|
